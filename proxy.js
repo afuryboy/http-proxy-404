cadb56658bf6fc3c19f61e5130bfbdeec1501165
qiankun <qiankun@sensetime.com>
2019-11-05 10:58:13 +0800

    :sparkles: 移除无用代码、注释代码 && 增加友好提示
    对targetList配置中没有可用服务器进行错误提示，并强制切换targetList配置中的第一个

--- proxy.js
+++ proxy.js
@@ -9,27 +9,33 @@ var app = express();
 class Proxy404 {
   constructor(options={}) {
     this.options = options
-    //this.checkParams();
     var proxyOptions = {
       target: options.targetList[0],
       changeOrigin: options.changeOrigin || true,
       ws: options.ws || false,
       router: function(req) {
         var index = req.headers['proxy-index']
+        var noServeFlag = false
+        if (index === undefined) {
+          noServeFlag = true
+          console.log(chalk.bold.red(`当前targetList配置中没有可用服务器,请检查...`));
+          index = 0
+          console.log(chalk.bold.yellow(`http-proxy-404已经强制切换服务器到targetList配置中的第一个`));
+        }
         if (options.log || options.log === undefined) {
-          console.log(chalk.green(`当前接口: ${req.url} 代理到的服务器是: ${options.targetList[index]}`));
+          noServeFlag ? console.log(chalk.bold.yellow(`当前接口: ${req.url} 代理到的服务器是: ${options.targetList[index]}`)) :
+          console.log(chalk.bold.green(`当前接口: ${req.url} 代理到的服务器是: ${options.targetList[index]}`));
         }
         return options.targetList[index]
       }
     }
-    
     this.proxy = proxy(proxyOptions)
     this.start()
-    console.log(chalk.green(`Serving!`));
+    console.log(chalk.bold.green(`http-proxy-404 is Serving!`));
 
-    console.log(chalk.green(`- Local:            http://localhost:${options.port} `));
+    console.log(chalk.bold.green(`- Local:            http://localhost:${options.port} `));
 
-    console.log(chalk.green(`- On Your Network:  http://${ipAdress}:${options.port}`));
+    console.log(chalk.bold.green(`- On Your Network:  http://${ipAdress}:${options.port} \n`));
     app.listen(options.port)
   }
   start() {
@@ -42,15 +48,6 @@ class Proxy404 {
     })
     app.all(this.options.apiReg,async(req,res,next) => {
       let mock
-      // let axiosOpt
-      // axiosOpt = {
-      //   url: this.currentTarget + req.url,
-      //   method: req.method,
-      // }
-      // let result = await axios(axiosOpt)
-      // if (result === 404) {
-      //   req.headers['proxy-index'] = true
-      // }
       var i = 0
       while(i<this.options.targetList.length) {
         let result = await axios({
@@ -67,13 +64,5 @@ class Proxy404 {
     })
     app.use(this.options.apiReg,this.proxy)
   }
-  // checkParams() {
-  //   if(!this.options.port) {
-  //     throw `port is required`
-  //   }
-  //   if(!this.options.targetList || this.options.targetList.length === 0) {
-  //     throw `targetList is required`
-  //   }
-  // }
 }
 module.exports = Proxy404
